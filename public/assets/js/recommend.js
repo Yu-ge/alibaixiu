4b1e80fc872a8769c5132ef4e232a19997f7359f
gaoyu <gaoyu251239@gmail.com>
2019-08-30 11:15:45 +0800

    完成了随机推荐和最新评论功能

--- public/assets/js/recommend.js
+++ public/assets/js/recommend.js
@@ -17,3 +17,62 @@ $.ajax({
        $('#recommendBox').html(html)
     }   
   })
+
+  //随机推荐
+  $.ajax({
+      url:'/posts/random',
+      type:'get',
+      success:function(res){
+          console.log(res);
+          
+          var str = `
+          {{each list}}
+          <li>
+          <a href="detail.html?id={{$value._id}}">
+            <p class="title">{{$value.title}}</p>
+            <p class="reading">阅读({{$value.meta.views}})</p>
+            <div class="pic">
+              <img src="{{$value.thumbnail}}" alt="">
+            </div>
+          </a>
+          </li>
+          {{/each}}
+          `
+          var html = template.render(str,{list:res})
+          $('#ramdoms').html(html)
+      }
+  })
+  template.defaults.imports.dataFormat = dataFormat;
+  //最新评论
+  $.ajax({
+      type:'get',
+      url:'/comments/lasted',
+      success:function(res){
+          var str = `
+          {{each list}}
+          <li>
+          <a href="javascript:;">
+            <div class="avatar">
+              <img src="{{$value.author.avatar}}" alt="">
+            </div>
+            <div class="txt">
+              <p>
+                <span>{{$value.author.nickName}}</span>{{dataFormat($value.createAt)}}说:
+              </p>
+              <p>{{$value.content}}</p>
+            </div>
+          </a>
+        </li>
+        {{/each}}
+          `
+         var html =  template.render(str,{list:res})
+         $('#commentBox').html(html)
+      }
+  })
+
+  function dataFormat(data) {
+    var nDate = new Date(data);
+    // padStart(字符串的位数,需要填充的字符串) 这个方法是属于字符串的方法 它主要作用就是用于字符串前面填充字符串  
+    var str = nDate.getFullYear() + '-' + (nDate.getMonth() + 1).toString().padStart(2, 0) + (nDate.getDate()).toString().padStart(2, 0);
+    return str;
+  }
